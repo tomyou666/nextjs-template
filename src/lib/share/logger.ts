7639bc4ab2022d0aeb643c7e584e02bb137df177
tomyou <tomyou625@yahoo.co.jp>
2025-03-24 01:01:33 +0900

    Vercelの環境変数によってログの出力先を変更

--- src/lib/share/logger.ts
+++ src/lib/share/logger.ts
@@ -12,48 +12,53 @@ export const logger = pino(
 	{
 		level: logLevel,
 		timestamp: () => `,"time":"${new Date(Date.now()).toISOString()}"`,
-		transport: {
-			targets: [
-				{
-					level: logLevel,
-					target: 'pino-pretty',
-					options: LOGGER_CONSTANTS.PRETTY_OPTIONS,
-				},
-				{
-					level: logLevel,
-					target: 'pino-roll',
-					options: {
-						file: LOGGER_CONSTANTS.LOG_FILE_PATH,
-						mkdir: true,
-						size: LOGGER_CONSTANTS.LOG_FILE_SIZE,
-						limit: {
-							count: LOGGER_CONSTANTS.LOG_FILE_COUNT,
+		// Vercelの環境変数によってログの出力先を変更
+		transport: process.env.VERCEL
+			? undefined
+			: {
+					targets: [
+						{
+							level: logLevel,
+							target: 'pino-pretty',
+							options: LOGGER_CONSTANTS.PRETTY_OPTIONS,
 						},
-						dateFormat: LOGGER_CONSTANTS.LOG_DATE_FORMAT,
-					},
+						{
+							level: logLevel,
+							target: 'pino-roll',
+							options: {
+								file: LOGGER_CONSTANTS.LOG_FILE_PATH,
+								mkdir: true,
+								size: LOGGER_CONSTANTS.LOG_FILE_SIZE,
+								limit: {
+									count: LOGGER_CONSTANTS.LOG_FILE_COUNT,
+								},
+								dateFormat: LOGGER_CONSTANTS.LOG_DATE_FORMAT,
+							},
+						},
+					],
 				},
-			],
-		},
 		browser: {
 			// see https://github.com/pinojs/pino/issues/1795
 			// write: () => {}, // ブラウザのコンソールにログを出力しないようにする
 			// ブラウザやミドルウェアのログをサーバーに送信するための設定
-			transmit: {
-				send: (level, logEvent) => {
-					// childを使用する場合にはlogEvent.messagesだけでなく、bindingsもサーバーに送信する必要がある
-					const messages = logEvent.messages
-					// ミドルウェアではnavigator.sendBeaconは使用できないため、keepalive:true の fetch を使用
-					// /api/logにリクエスト
-					void fetch(logUrl, {
-						method: 'POST',
-						headers: {
-							'Content-Type': 'application/json',
+			transmit: process.env.VERCE
+				? undefined
+				: {
+						send: (level, logEvent) => {
+							// childを使用する場合にはlogEvent.messagesだけでなく、bindingsもサーバーに送信する必要がある
+							const messages = logEvent.messages
+							// ミドルウェアではnavigator.sendBeaconは使用できないため、keepalive:true の fetch を使用
+							// /api/logにリクエスト
+							void fetch(logUrl, {
+								method: 'POST',
+								headers: {
+									'Content-Type': 'application/json',
+								},
+								body: JSON.stringify({ level, messages }),
+								keepalive: true,
+							})
 						},
-						body: JSON.stringify({ level, messages }),
-						keepalive: true,
-					})
-				},
-			},
+					},
 		},
 	},
 	// prettyStream,
